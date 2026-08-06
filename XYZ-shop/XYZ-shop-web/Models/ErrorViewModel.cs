9cb73c96bf7021173da444806e00a33d19f3ea51
DESKTOP-NIKITA\Nikita Zholnerovich <nikita.zholnerovich21@gmail.com>
2026-08-06 14:49:47 +0300

    Delete excess project

--- XYZ-shop/XYZ-shop-web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace XYZ_shop_web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
